0594f83bbf43bd139c98df327e14b33b8bfb14a4
Lebowski <joker13504@gmail.com>
2025-07-30 22:04:02 +0300

    new

---
+++ script/index.js
@@ -0,0 +1,3 @@
+const button =  document.querySelector('.button_main')
+const buttonText = document.querySelector('.button_text')
+const buttonTemplate = document.querySelector('#button-template').content 
